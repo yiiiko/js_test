7f46f2b4453e62172b2b6d753c7f1f7538160192
yiiiko <yikoli@yiko.wireless.newschool.edu>
2019-11-12 16:19:58 -0500

    1

--- sketch.js
+++ sketch.js
@@ -1,20 +1,30 @@
-let img;
+var img;
+
+function preload(){
+  img0 = loadImage('assets/map.png');
+  // img1 = loadImage('assets/NY.png');
+  // img2 = loadImage('assets/TX.png');
+  // img3 = loadImage('assets/KY.png');
+  // img4 = loadImage('assets/OH.png');
+  // img5 = loadImage('assets/NE.png');
+}
+
 function setup() {
   createCanvas(710, 400);
-  img0 = loadImage('assets/map.png');
-  img1 = loadImage('assets/NY.png');
-  img2 = loadImage('assets/TX.png');
-  img3 = loadImage('assets/KY.png');
-  img4 = loadImage('assets/OH.png');
-  img5 = loadImage('assets/NE.png');
 }
 
 function draw() {
+  textSize(32);
+text('word', 10, 30);
+fill(0, 102, 153);
+text('word', 10, 60);
+fill(0, 102, 153, 51);
+text('word', 10, 90);
   image(img0, 0, 0);
-  image(img1, 0, 0);
-  image(img2, 0, 0);
-  image(img3, 0, 0);
-  image(img4, 0, 0);
-  image(img5, 0, 0);
+  // image(img1, 0, 0);
+  // image(img2, 0, 0);
+  // image(img3, 0, 0);
+  // image(img4, 0, 0);
+  // image(img5, 0, 0);
 
 }
